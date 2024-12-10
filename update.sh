2d4fde6bf0d9fab630f81c387b484f25c2a789df
BigBinnie <yaobinwei@sjtu.edu.cn>
2024-12-10 00:16:19 -0600

    Initial commit

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ git add .
 git commit -m "Initial commit"
 git push -u origin main
 
-hugo # Generate the website inside the public directory
+../go/bin/hugo # Generate the website inside the public directory
 cd public
 git add .
 git commit -m "Build website"
